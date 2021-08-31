4645b23d75e733a65bd6b2dfc327a2ef3efb5e79
Domok23 <wahyur2bro@gmail.com>
2021-08-31 21:50:10 +0700

    tampilan kampung di home

--- application/views/home/kampung.php
+++ application/views/home/kampung.php
@@ -2,14 +2,14 @@
 <div class="container">
     <div class="row">
         <div class="our-services-option">
-            <div class="section-header" data-aos="fade-down" data-aos-duration="1500" data-aos-offset="300">
+            <div class="section-header" data-aos="fade-down" data-aos-duration="1500" data-aos-offset="200">
                 <h2>Kampung Wisata <br>di <?php echo $site->namaweb ?></h2>
             </div>
             <!-- .section-header -->
             <div class="row">
                 <?php foreach($kampung as $kampung) { ?>
                 <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
-                    <div class="our-services-box" data-aos="zoom-in" data-aos-duration="500">
+                    <div class="our-services-box" data-aos="zoom-in" data-aos-duration="500" data-aos-offset="300">
                         <div class="our-services-items">
                             <i class="<?php echo $kampung->icon ?> fa-5x" style="color:#337ab7; margin-bottom: 20px;"></i>
                             <div class="our-services-content">
@@ -26,12 +26,12 @@
                 <?php } ?>
             </div>
             <div class="donation-section">
-                <div class="donaton-header" data-aos="fade-down" data-aos-duration="1500" data-aos-offset="300">
+                <div class="donaton-header" data-aos="fade-down" data-aos-duration="1500" data-aos-offset="200">
                     <h3>UMKM Desa Wisata Purbayan</h3>
                 </div>
                 <!-- .count-number -->
-                <div class="donate-btn" data-aos="zoom-in" data-aos-duration="500">
-                <a href="donate.html" class="btn btn-default">Lihat UMKM <i class="fa fa-heart" aria-hidden="true"></i></a>
+                <div class="donate-btn" data-aos="zoom-in" data-aos-duration="500" data-aos-offset="300">
+                <a href="donate.html" class="btn btn-default">Lihat UMKM <i class="fad fa-angle-right" aria-hidden="true"></i></a>
                 </div>
                 <!-- .donate-option -->
             </div>
